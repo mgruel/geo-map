75d322a9b416e670717b9e97ca9117f327ba7c0c
Meykel Gruel <developer@gruel.eu>
2021-07-07 12:52:06 +0200

    Initial commit

---
+++ src/_components/JumpTo.svelte
@@ -0,0 +1,78 @@
+<script>
+	import { getContext } from 'svelte';
+	import L from 'leaflet';
+	
+	const { getMap } = getContext(L);
+
+	L.Control.JumpTo = L.Control.extend({
+		options: {
+			position: 'topleft'
+		},
+
+		onAdd: function () {
+			const className = 'leaflet-control-jump-to';
+			const container = L.DomUtil.create('div', className);
+
+      this._addLabel(`${className}-label`, container);
+			this._addInput(`${className}-input`, container);
+
+			return container;
+		},
+
+    _addLabel: function (className, container) {
+      this._label = L.DomUtil.create('span', className, container);
+      this._label.innerHTML = 'Jump to:';
+    },
+
+		_addInput: function (className, container) {
+			this._input = L.DomUtil.create('input', className, container);
+      this._input.addEventListener('keydown', this._update.bind(this));
+		},
+
+		_update: function (event) {
+			if (event.key === "Enter") {
+        const coords = this?._input.value.split(',');
+        console.debug(coords);
+        if (coords.length >= 2) {
+          this._map.flyTo(coords, this._map.getZoom(), { animate: true });
+          this._input.value = null;
+        }
+      }
+		},
+
+		onRemove: function () {
+			this._input.removeEventListener('keydown', this._update);
+		}
+	});
+
+	L.control.jumpTo = function(options) {
+		return new L.Control.JumpTo(options);
+	};
+	
+	L.control.jumpTo({ position: 'topleft' }).addTo(getMap());
+</script>
+
+<svelte:head>
+	<style>
+    .leaflet-control-jump-to {
+      background: rgba(255, 255, 255, 1.0);
+      border: 0;
+			border-bottom: 1px solid black;
+			border-right: 1px solid black;
+			padding: 0 3px;
+      
+			margin-top: 0 !important;
+			margin-bottom: 0 !important;
+			margin-left: 0 !important;
+			margin-right: 0 !important;
+    }
+		.leaflet-control-jump-to-input,
+    .leaflet-control-jump-to-input:active,
+    .leaflet-control-jump-to-input:focus {
+      outline: none;
+			border: 0;
+			padding: 2px 5px 1px;
+			border-radius: 3px;
+		}
+	</style>
+</svelte:head>
---
+++ src/_components/TileLayer.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { getContext } from 'svelte';
+	import L from 'leaflet';
+	
+	const { getMap } = getContext(L);
+	
+	const createCartoUrl = (style) => `https://{s}.basemaps.cartocdn.com/${style}/{z}/{x}/{y}${L.Browser.retina ? '@2x.png' : '.png'}`;
+	const cartoConfig = {
+		attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+		subdomains: 'abcd',
+		minZoom: 7,
+		maxZoom: 20
+	};
+
+	const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		subdomains: 'abc',
+		minzoom: 7,
+		maxZoom: 19
+	});
+	const voyager = L.tileLayer(createCartoUrl('rastertiles/voyager'), cartoConfig).addTo(getMap());
+	// const image = L.imageOverlay('https://cdn.gruel.cloud/map.png', [[52.9141, 5.1152], [52.7456, 4.8951]]).addTo(getMap());
+
+	// L.control.layers({ osm, voyager }, {}, { collapsed: true, hideSingleBase: true, position: 'topright' }).addTo(getMap());
+</script>
