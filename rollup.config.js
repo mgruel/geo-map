e13d521a29892ae5d269ab0af9b169dda5137b4e
Meykel Gruel <developer@gruel.eu>
2022-09-28 03:26:37 +0200

    feature: Change rollup config to allow zipping of bundled code

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import clear from 'rollup-plugin-clear';
+import copy from 'rollup-plugin-copy2';
+import zipPlugin from 'rollup-plugin-zip';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -34,9 +37,14 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'dist/bundle.js'
 	},
 	plugins: [
+    clear({
+      targets: ['dist'],
+      watch: true,
+    }),
+
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
@@ -58,17 +66,35 @@ export default {
 		}),
 		commonjs(),
 
+    copy({
+      assets: [
+        ['public/favicon.png', 'favicon.png'],
+        ['public/global.css', 'global.css'],
+        ['public/index.html', 'index.html'],
+        ['node_modules/leaflet/dist/leaflet.css', 'leaflet.css'],
+        ['node_modules/leaflet/dist/images/layers-2x.png', 'images/layers-2x.png'],
+        ['node_modules/leaflet/dist/images/layers.png', 'images/layers.png'],
+        ['node_modules/leaflet/dist/images/marker-icon-2x.png', 'images/marker-icon-2x.png'],
+        ['node_modules/leaflet/dist/images/marker-icon.png', 'images/marker-icon.png'],
+        ['node_modules/leaflet/dist/images/marker-shadow.png', 'images/marker-shadow.png'],
+      ]
+    }),
+
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
 		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('dist'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+    // production && zipPlugin({
+    //   dir: 'dist',
+    // }),
 	],
 	watch: {
 		clearScreen: false
