701bb30552e97660a713757ffeda21a1b8c52131
Meykel Gruel <developer@gruel.eu>
2025-05-18 10:09:28 +0200

    feature: Added Cyclosm tile-server

--- src/components/TileLayer.svelte
+++ src/components/TileLayer.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
     import L from "leaflet";
-    import { getContext } from "svelte";
+    import {getContext} from "svelte";
     
     const { getMap } = getContext<{ getMap: () => L.Map }>(L);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
         subdomains: "abc",
         minZoom: 7,
         maxZoom: 19
     }).addTo(getMap());
 
-    // L.control.layers({ osm }, {}, { collapsed: true, hideSingleBase: true, position: "topright" }).addTo(getMap());
+    const cycling = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &amp; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/">Cyclosm</a>`,
+        subdomains: "abc",
+        minZoom: 7,
+        maxZoom: 17,
+    });
+
+    L.control.layers({ osm, cycling }, {}, { collapsed: true, hideSingleBase: true, position: "topright" }).addTo(getMap());
 </script>
