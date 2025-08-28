e7cf56f193aa9fff6fbaea263a7616df4f23f44e
Meykel Gruel <developer@gruel.eu>
2025-08-28 12:14:07 +0200

    chore: Updated dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -51,6 +51,7 @@ export default {
         dev: !production
       }
     }),
+
     // we'll extract any component CSS out into
     // a separate file - better for performance
     css({ output: 'bundle.css' }),
